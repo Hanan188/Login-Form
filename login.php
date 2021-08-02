9002445c1b89571fd1e492f284c8921685e7d7a9
Hanan188 <85380973+Hanan188@users.noreply.github.com>
2021-08-02 23:08:15 +0300

    Add files via upload

---
+++ login.php
@@ -0,0 +1,9 @@
+<?php
+//extract a data using POST method
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+//display extracted data on screen
+echo "Username : ".$username."<br>";
+echo "Password : ".$password;
+?>
